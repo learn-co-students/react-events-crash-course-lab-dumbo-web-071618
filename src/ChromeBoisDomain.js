0e23a061a5b0dfba087b8b4fe9d9b3a427794f83
github username <github email address>
2018-09-18 14:30:54 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,31 +3,62 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+
   handleMouseMove = (event) => {
+    // console.log("clientX" + event.clientX);
+    // console.log("clientY" + event.clientY);
+
+    // console.log("movementX" + event.movementX);
+    // console.log("movementY" + event.movementY);
+
+    // console.log("pageX" + event.pageX);
+    // console.log("pageY" + event.pageY);
+
+    const x = event.clientX
+    const y = event.clientY
+    drawChromeBoiAtCoords(x, y);
+
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
   }
-  
+
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+  handleOnClick = (event) => {
+    toggleCycling()
+  }
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
+  /* if the key pressed was 's', then it should call `resize` with '-'
    */
-  
+   //onKeyDown onKeyPress onKeyUp
+
+  // handleOnKeyPress
+  // handleOnKeyUP
+  handleOnKeyDown = (event) => {
+    // console.log(event.charCode);
+    // console.log(event.key);
+    if (event.key === 'a'){
+      resize('+')
+    } else if(event.key === 's'){
+      resize('-')
+    }
+  }
+
   render() {
     return (
-      <canvas 
+      <canvas
         onMouseMove={this.handleMouseMove}
+        onClick={this.handleOnClick}
+        onKeyDown={this.handleOnKeyDown}
         width='900'
         height='600'
         tabIndex="0">
