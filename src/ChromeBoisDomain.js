8581549d407e9016530bcee389bc41f83b69d2e1
Nkosi Enciso Givhan <codeatblack@gmail.com>
2018-09-20 17:22:38 -0400

    working draw state with working brush strokes and stroke hz

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,24 +3,23 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-
-  handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
-     drawChromeBoiAtCoords(event.clientX, event.clientY)
+  constructor(){
+    super()
+    this.state = {
+      imgPNG: "http://localhost:3000/1.png",
+      mouseDown: false,
+      percentage: 1
+    }
   }
 
-  handleToggleCycle = (event) => {
-    toggleCycling()
+
+
+  percentageIncrease = () => {
+    this.setState(prevState => {
+      return {percentage: prevState.percentage + 1}
+    })
   }
 
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
-   */
 
   handleKeyDown = (event) => {
     if (event.key === "a"){
@@ -31,23 +30,134 @@ export default class ChromeBoisDomain extends Component {
     }
   }
 
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-'
-   */
+  handleEmojiSelect = (event) => {
+    let id = event.target.id
+    this.setState({imgPNG: `http://localhost:3000/${id}.png`}, () => {
+
+    })
+  }
+
+  percentageReset = () =>{
+    this.setState({percentage: 2})
+  }
+
+  handleMouseDown = (event) => {
+    this.percentageReset
+    this.setState(prevState => {
+      return {mouseDown: !prevState.mouseDown}
+    })
+  }
+
+  handleMouseUp = (event) => {
+    this.percentageReset
+    this.setState(prevState => {
+      return {mouseDown: !prevState.mouseDown}
+    })
+  }
+
+  handleMouseMove = (event) => {
+    this.percentageIncrease()
+    console.log(this.state.percentage);
+    if (this.state.mouseDown){
+      if(this.state.percentage % 2 === 0){
+        drawChromeBoiAtCoords(event.clientX, event.clientY, this.state.imgPNG)
+      }
+    }
+  }
+
 
   render() {
     return (
+    <div>
+    <div>
       <canvas
         onMouseMove={this.handleMouseMove}
-        onClick={this.handleToggleCycle}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
         onKeyDown={this.handleKeyDown}
-        width='900'
+        width='955'
         height='600'
         tabIndex="0">
       </canvas>
+
+      </div>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+      <text> <br></br> </text>
+
+
+      <div class="container" >
+        <img id = "1" src="1.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "2" src="2.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "3" src="3.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "4" src="4.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "5" src="5.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "6" src="6.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "7" src="7.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "8" src="8.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "9" src="9.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "10" src="10.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "11" src="11.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "12" src="12.png" alt=""onClick={this.handleEmojiSelect}/>
+        <text> <br></br> </text>
+
+        <img id = "13" src="13.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "14" src="14.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "15" src="15.png" alt=""onClick={this.handleEmojiSelect}/>
+
+        <img id = "16" src="16.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "17" src="17.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "18" src="18.png" alt=""onClick={this.handleEmojiSelect}/>
+
+        <img id = "19" src="19.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "20" src="20.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "21" src="21.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "22" src="22.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "23" src="23.png" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "24" src="24.png" alt=""onClick={this.handleEmojiSelect}/>
+
+        <text> <br></br> </text>
+        <text> <br></br> </text>
+
+        <img id = "horizontal" src="horizontal.svg" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "vertical" src="vertical.svg" alt=""onClick={this.handleEmojiSelect}/>
+        <img id = "clockwise" src="clockwise.svg" alt=""onClick={this.handleEmojiSelect}/>
+
+
+      </div>
+    </div>
+
     )
   }
 }
--- src/canvasHelpers.js
+++ src/canvasHelpers.js
@@ -7,42 +7,33 @@ let cycling = false
 let idx = 0
 let [sizeX, sizeY] = [95, 121]
 
-export function init() {
-  
-  def = document.createElement("img")
-  const green = document.createElement("img")
-  const red = document.createElement("img")
-  const yellow = document.createElement("img")
 
-  def.src = "chrome-boi.png"
-  green.src = "chrome-boi-green.png"
-  red.src = "chrome-boi-red.png"
-  yellow.src = "chrome-boi-yellow.png"
-  
-  colors = [green, red, yellow]
+export function init() {
+
+  def = document.createElement("img")
+  def.src = "1.png"
 }
 
-export function drawChromeBoiAtCoords(x, y) {
-  
+export function drawChromeBoiAtCoords(x, y, imgPNG) {
+
   const canvas = document.querySelector("canvas") // sloppy but we haven't introduced lifecycle methods and canvas wouldn't be rendered
   const ctx = canvas.getContext("2d")
   const rect = canvas.getBoundingClientRect()
   const [cX, cY] = [rect.left, rect.top]
-  
+
   let img
-  if (cycling) {
+  if (false) {
     img = colors[idx]
     idx = (idx + 1) % 3
   } else {
+    def.src = imgPNG
+
     img = def
   }
-  
+
   ctx.drawImage(img, x - cX - 50, y - cY - 80, sizeX, sizeY)
 }
 
-export function toggleCycling() {
-  cycling = !cycling
-}
 
 export function resize(type) {
   const multiplier = (type === "+") ? 1.1 : 0.9
